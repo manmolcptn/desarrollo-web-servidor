065e11b2759c531330de033a9855b736d7bcf337
manmolcptn <molinacapitanmiguelangel@gmail.com>
2024-11-02 11:57:28 +0100

    Repaso de inicio de Php

--- 01_Introduccion/index.php
+++ 01_Introduccion/index.php
@@ -4,15 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
+    <?php
+        //  Activamos los errores de PHP
+        error_reporting( E_ALL );
+        ini_set( "display_errors", 1 );
+    ?>
 </head>
 <body>
     <?php
+    //Define se usa para definir constantes
     define("EDAD",25);
 
     $var = "hola mundo";
     //echo $var;
-    var_dump($var);
-
+    //var_dump es como un echo pero mucho mas detallado 
+    var_dump($var); 
+    
+    
     $var = 3;
     var_dump($var);
 
@@ -25,7 +33,7 @@
     $frase1 = "hola";
     $frase2 = "mundo";
 
-    echo "<p>$frase1 " . "$frase2</p>";
+    echo "<p>$frase1 $frase2</p>";
 
     echo $frase1 . $frase2;
     ?>
